bd9f29a8e5b93f9a816bd95bebddf3c058e960b7
razcohen5 <33660332+razcohen5@users.noreply.github.com>
2018-05-29 22:14:02 +0300

    Add files via upload

--- Board.cpp
+++ Board.cpp
@@ -43,24 +43,24 @@
 		b=t;
 	}
 
-	// const Bridge Board :: operator[](const Coordinate& c) 
-	// {
-	// 	cout<<"not const";
-	// 	if(c.row<0||c.col<0||c.row>=size||c.col>=size)
-	// 		throw IllegalCoordinateException(c.row,c.col);
-	// 	else if(b[c.row][c.col]=='O'||b[c.row][c.col]=='X')
-	// 		throw IllegalCoordinateException(c.row,c.col);
-	// 	else
-	// 		return Bridge(this,c.row,c.col);
-	// }
+	const Bridge Board :: operator[](const Coordinate& c) 
+	{
+		if(c.row<0||c.col<0||c.row>=size||c.col>=size)
+			throw IllegalCoordinateException(c.row,c.col);
+		else if(b[c.row][c.col]=='O'||b[c.row][c.col]=='X')
+			throw IllegalCoordinateException(c.row,c.col);
+		else
+			return Bridge(this,c.row,c.col);
+	}
 
 	const Bridge Board :: operator[](const Coordinate& c) const
 	{
-		//if(c.row<0||c.col<0||c.row>=size||c.col>=size)
-			//throw IllegalCoordinateException(c.row,c.col);
-		//else if(b[c.row][c.col]!='.')
-		 //	throw IllegalCoordinateException(c.row,c.col);	
-		return Bridge(this,c.row,c.col);
+		if(c.row<0||c.col<0||c.row>=size||c.col>=size)
+			throw IllegalCoordinateException(c.row,c.col);
+		else if(b[c.row][c.col]=='O'||b[c.row][c.col]=='X')
+			throw IllegalCoordinateException(c.row,c.col);
+		else
+			return Bridge(this,c.row,c.col);
 	}
 
 	void Board :: operator=(char c)
@@ -93,77 +93,229 @@
 		b=t;
 	}
 
-	inline bool exists(const std::string& name)
-	{
-		ifstream f(name.c_str());
-		return f.good();
-	}
-
 	string Board :: draw(int n)
 	{
-		//check if file already exist and if it is create a new one
-		string filename = "TicTacToe.ppm";
-		for(int i=0;exists(filename);i++)
-			filename = "TicTacToe" + to_string(i) + ".ppm";
+		string filename = "TicTacToe5.ppm";
 		ofstream imageFile(filename, ios::out | ios::binary);
 		imageFile << "P6" << endl << n <<" " << n << endl << 255 << endl;
 		RGB *image = new RGB[n*n];
-
-		int *PixelsRange = new int[size+1];
-		int sum=0;
-		int q;
-		PixelsRange[0] = 0;
-		for(q=1;q<size;q++)
-		{
-			PixelsRange[q] = n/size + sum;
-			sum = PixelsRange[q];
-		}
-		PixelsRange[q] = n;
-
 		int row = 0;
   		int col = 0;
-		while(row < size)
+		for(int j=0;j<n/3;++j)
 		{
-			for(int j=PixelsRange[row];j<PixelsRange[row+1];++j)
+			for(int i=0;i<n/3;++i)
 			{
-				while(col < size)
+				if(b[row][col]=='X')
 				{
-					for(int i=PixelsRange[col];i<PixelsRange[col+1];++i)
-					{
-						if(b[row][col]=='X')
-						{
-							image[n*j+i].red = 102;
-							image[n*j+i].green = 178;
-							image[n*j+i].blue = 255;
-						}
-						else if(b[row][col]=='O')
-						{
-							image[n*j+i].red = 102;
-							image[n*j+i].green = 255;
-							image[n*j+i].blue = 178;
-						}
-						else
-						{
-							image[n*j+i].red = 255;
-							image[n*j+i].green = 255;
-							image[n*j+i].blue = 255;
-						}
-					}
-					col++;
+					image[n*j+i].red = 102;
+					image[n*j+i].green = 178;
+					image[n*j+i].blue = 255;
 				}
-				col = 0;
+				else if(b[row][col]=='O')
+				{
+					image[n*j+i].red = 102;
+					image[n*j+i].green = 255;
+					image[n*j+i].blue = 178;
+				}
+				else
+				{
+					image[n*j+i].red = 255;
+					image[n*j+i].green = 255;
+					image[n*j+i].blue = 255;
+				}
 			}
-			row++;
-			col = 0;			
+			col++;
+			for(int i = n/3;i<2*(n/3);++i)
+			{
+				if(b[row][col]=='X')
+				{
+					image[n*j+i].red = 102;
+					image[n*j+i].green = 178;
+					image[n*j+i].blue = 255;
+				}
+				else if(b[row][col]=='O')
+				{
+					image[n*j+i].red = 102;
+					image[n*j+i].green = 255;
+					image[n*j+i].blue = 178;
+				}
+				else
+				{
+					image[n*j+i].red = 255;
+					image[n*j+i].green = 255;
+					image[n*j+i].blue = 255;
+				}
+			}
+			col++;
+			for(int i = 2*(n/3);i<n;++i)
+			{
+				if(b[row][col]=='X')
+				{
+					image[n*j+i].red = 102;
+					image[n*j+i].green = 178;
+					image[n*j+i].blue = 255;
+				}
+				else if(b[row][col]=='O')
+				{
+					image[n*j+i].red = 102;
+					image[n*j+i].green = 255;
+					image[n*j+i].blue = 178;
+				}
+				else
+				{
+					image[n*j+i].red = 255;
+					image[n*j+i].green = 255;
+					image[n*j+i].blue = 255;
+				}
+			}
+			col=0;
 		}
-
+		row++;
+		for(int j=n/3;j<2*(n/3);++j)
+		{
+			for(int i=0;i<n/3;++i)
+			{
+				if(b[row][col]=='X')
+				{
+					image[n*j+i].red = 102;
+					image[n*j+i].green = 178;
+					image[n*j+i].blue = 255;
+				}
+				else if(b[row][col]=='O')
+				{
+					image[n*j+i].red = 102;
+					image[n*j+i].green = 255;
+					image[n*j+i].blue = 178;
+				}
+				else
+				{
+					image[n*j+i].red = 255;
+					image[n*j+i].green = 255;
+					image[n*j+i].blue = 255;
+				}
+			}
+			col++;
+			for(int i = n/3;i<2*(n/3);++i)
+			{
+				if(b[row][col]=='X')
+				{
+					image[n*j+i].red = 102;
+					image[n*j+i].green = 178;
+					image[n*j+i].blue = 255;
+				}
+				else if(b[row][col]=='O')
+				{
+					image[n*j+i].red = 102;
+					image[n*j+i].green = 255;
+					image[n*j+i].blue = 178;
+				}
+				else
+				{
+					image[n*j+i].red = 255;
+					image[n*j+i].green = 255;
+					image[n*j+i].blue = 255;
+				}
+			}
+			col++;
+			for(int i = 2*(n/3);i<n;++i)
+			{
+				if(b[row][col]=='X')
+				{
+					image[n*j+i].red = 102;
+					image[n*j+i].green = 178;
+					image[n*j+i].blue = 255;
+				}
+				else if(b[row][col]=='O')
+				{
+					image[n*j+i].red = 102;
+					image[n*j+i].green = 255;
+					image[n*j+i].blue = 178;
+				}
+				else
+				{
+					image[n*j+i].red = 255;
+					image[n*j+i].green = 255;
+					image[n*j+i].blue = 255;
+				}
+			}
+			col=0;
+		}
+		row++;
+		for(int j=2*(n/3);j<n;++j)
+		{
+			for(int i=0;i<n/3;++i)
+			{
+				if(b[row][col]=='X')
+				{
+					image[n*j+i].red = 102;
+					image[n*j+i].green = 178;
+					image[n*j+i].blue = 255;
+				}
+				else if(b[row][col]=='O')
+				{
+					image[n*j+i].red = 102;
+					image[n*j+i].green = 255;
+					image[n*j+i].blue = 178;
+				}
+				else
+				{
+					image[n*j+i].red = 255;
+					image[n*j+i].green = 255;
+					image[n*j+i].blue = 255;
+				}
+			}	
+			col++;
+			for(int i = n/3;i<2*(n/3);++i)
+			{
+				if(b[row][col]=='X')
+				{
+					image[n*j+i].red = 102;
+					image[n*j+i].green = 178;
+					image[n*j+i].blue = 255;
+				}
+				else if(b[row][col]=='O')
+				{
+					image[n*j+i].red = 102;
+					image[n*j+i].green = 255;
+					image[n*j+i].blue = 178;
+				}
+				else
+				{
+					image[n*j+i].red = 255;
+					image[n*j+i].green = 255;
+					image[n*j+i].blue = 255;
+				}
+			}
+			col++;
+			for(int i = 2*(n/3);i<n;++i)
+			{
+				if(b[row][col]=='X')
+				{
+					image[n*j+i].red = 102;
+					image[n*j+i].green = 178;
+					image[n*j+i].blue = 255;
+				}
+				else if(b[row][col]=='O')
+				{
+					image[n*j+i].red = 102;
+					image[n*j+i].green = 255;
+					image[n*j+i].blue = 178;
+				}
+				else
+				{
+					image[n*j+i].red = 255;
+					image[n*j+i].green = 255;
+					image[n*j+i].blue = 255;
+				}
+			}
+			col=0;
+		}
 		image[0].red = 255;
  		image[0].blue = 0;
   		image[0].green = 0;
 		imageFile.write(reinterpret_cast<char*>(image), 3*n*n);
  		imageFile.close();
 		delete[] image;
-		delete[] PixelsRange;
 		return filename;
 	}
 
@@ -184,31 +336,13 @@
 
 	istream& operator >> (istream& in, Board& b)
 	{
-		b.deleteBoard();
-		string line = "";
-		in >> line;
-		b.size = line.length();
-		char **t = new char* [b.size];
 		for(int i=0;i<b.size;i++)
-			t[i] = new char[b.size];
-
-		for(int i=0;i<b.size;i++)
-			for(int j=0;j<b.size;j++)
-				t[i][j] = '.';
-		b.b=t;
-
-		int i;
-		int j;
-		for(i=0;i<b.size-1;i++)
 		{		
-			for(j=0;j<b.size;j++)
-				b.b[i][j] = line[j];
-			line = "";
+			string line = "";
 			in >> line;
+			for(int j=0;j<b.size;j++)
+				b.b[i][j] = line[j];
 		}
-		for(j=0;j<b.size;j++)
-			b.b[i][j] = line[j];
-
 		return in;
 	}
 
@@ -237,10 +371,6 @@
 	{
 		if(c!='X'&&c!='O'&&c!='.')
 			throw IllegalCharException(c);
-		if(i<0||j<0||i>=bp->size||j>=bp->size)
-			throw IllegalCoordinateException(i,j);
-		if(bp->b[i][j]!='.')
-			throw IllegalCoordinateException(i,j);	
 		bp->b[i][j] = c;
 	}
 
@@ -288,10 +418,10 @@
 		return c;
 	}
 
-	int main() {
+/*	int main() {
 	Board board;
 	cin >> board;
 	string filename = board.draw(600);
 	cout << filename << endl;
-}
+}*/
 	
